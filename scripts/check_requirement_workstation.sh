00b8ff6be9f4ab50028256c63a1e544436cf504d
fgerling <felix@fgerling.de>
2020-02-20 20:53:11 +0100

    Add requirement check script for the workstation

---
+++ scripts/check_requirement_workstation.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env sh
+fulfilled="true"
+
+command -v SUSEConnect >/dev/null || echo "SUSEConnect not found" 1>&2 && exit 2
+
+if ! (SUSEConnect --status | jq -er '.[] | select(.identifier=="caasp") | .subscription_status' | grep -q "ACTIVE")
+then
+	echo "SUSE CaaS Platform extension subscription is not ACTIVE" 1>&2
+	fulfilled=false
+fi
+if ! (SUSEConnect --status | jq -er '.[] | select(.identifier=="sle-module-containers") | .status' | grep -q "Registered")
+then
+	echo "sle-module-containers is not 'Registered'"
+	fulfilled=false
+fi
+if [ "$fulfilled" == "false" ]
+then
+	exit 1
+fi
